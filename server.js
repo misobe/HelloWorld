79e4b90c60467ea576fac0a1a085da7e857941dd
磯部 眞人 <ylem@ylem.local>
2011-11-04 16:43:30 +0900

    222

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Ninja World ninnin\n');
+  res.end('Hello World\n');
 }).listen(80);
 
